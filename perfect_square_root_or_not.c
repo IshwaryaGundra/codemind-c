0d34d2b070ac6a2018acc23ab158a9193be6a6e7
IshwaryaGundra <97424580+IshwaryaGundra@users.noreply.github.com>
2022-04-25 19:56:21 +0530

    #include<stdio.h> int ispersq(int n) {     int i,x,flag;//66     for(i=0;i<n;i++)     {         x=i*i;     if(x==n)     {         flag=1;         break;     }     }     if(flag==1)
        {
            return 1;
        }
        else
        {
            return 0;
        }
    }
    int main()
    {
        int n;
        scanf("%d",&n);
        if(ispersq(n))
        {
            printf("True");
        }
        else
        {
            printf("False");
        }
    }

--- perfect_square_root_or_not.c
+++ perfect_square_root_or_not.c
@@ -1,15 +1,36 @@
 #include<stdio.h>
+int ispersq(int n)
+{
+    int i,x,flag;//66
+    for(i=0;i<n;i++)
+    {
+        x=i*i;
+    if(x==n)
+    {
+        flag=1;
+        break;
+    }
+    }
+    if(flag==1)
+    
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
 int main()
 {
-    int a,n;
+    int n;
     scanf("%d",&n);
-    for(a=0;a<=n;a++)
+    if(ispersq(n))
     {
-        if(n==a*a)
-        {
-            printf("True");
-            return 0;
-        }
+        printf("True");
     }
-    printf("False");
+    else
+    {
+        printf("False");
+    }
 }
