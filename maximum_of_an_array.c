a07331349a872e285b1d84328532b61fa79a5394
IshwaryaGundra <97424580+IshwaryaGundra@users.noreply.github.com>
2022-04-27 20:43:22 +0530

    #include<stdio.h> int min(int*arr ,int n) {     int i,m=arr[0];     for(i=0;i<n;i++)     {     if(m<arr[i])     {         m=arr[i];
        }
        }
        return m;
    
    }
    int main()
    {
        int n,arr[100],i;
        scanf("%d",&n);
        for(i=0;i<n;i++)
        {
            scanf("%d",&arr[i]);
        }
        printf("%d",min(arr,n));
    }

---
+++ maximum_of_an_array.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int min(int*arr ,int n)
+{
+    int i,m=arr[0];
+    for(i=0;i<n;i++)
+    {
+    if(m<arr[i])
+    {
+        m=arr[i];
+    
+    }
+    }
+    return m;
+
+}
+int main()
+{
+    int n,arr[100],i;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    printf("%d",min(arr,n));
+}
