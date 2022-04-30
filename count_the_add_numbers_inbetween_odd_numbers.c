7a34b767f67a991054f7fccec434d2455f203301
IshwaryaGundra <97424580+IshwaryaGundra@users.noreply.github.com>
2022-04-30 10:13:05 +0530

    #include<stdio.h> int count(int n,int* arr) {     int i,c=0;     for(i=0;i<n-2;i++)     {         if((arr[i])%2!=0&& (arr[i+2])%2!=0)         {             if(arr[i+1]%2!=0)             {                 c++;             }         }     }     return c; } int main() {     int n,i,arr[100];     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     printf("%d",count(n,arr)); }

---
+++ count_the_add_numbers_inbetween_odd_numbers.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int count(int n,int* arr)
+{
+    int i,c=0;
+    for(i=0;i<n-2;i++)
+    {
+        if((arr[i])%2!=0&& (arr[i+2])%2!=0)
+        {
+            if(arr[i+1]%2!=0)
+            {
+                c++;
+            }
+        }
+    }
+    return c;
+}
+int main()
+{
+    int n,i,arr[100];
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    printf("%d",count(n,arr));
+}
