b471d077b746467a238015856491d018ebce956c
IshwaryaGundra <97424580+IshwaryaGundra@users.noreply.github.com>
2022-05-06 23:03:11 +0530

    #include<stdio.h> #include<math.h> int main() {     int n,i,arr[100],k,s=0,h=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n/2;i++)     {         s=s+arr[i];     }      for(i=n/2;i<n;i++)     {         h=h+arr[i];     }     printf("%d",abs(s-h));
    }

---
+++ difference_between_sum_of_first_half_and_second_half_in_an_array.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int n,i,arr[100],k,s=0,h=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n/2;i++)
+    {
+        s=s+arr[i];
+    }
+     for(i=n/2;i<n;i++)
+    {
+        h=h+arr[i];
+    }
+    printf("%d",abs(s-h));
+    
+}
